c3f1c9c50a48810137d2400bfe655cdbe54698f6
miso <michal.sobocinski@mimacom.com>
2017-06-28 08:23:56 +0200

    h2 config added

--- src/main/java/com/example/addressbook/ApplicationConfig.java
+++ src/main/java/com/example/addressbook/ApplicationConfig.java
@@ -1,5 +1,8 @@
 package com.example.addressbook;
 
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,6 +14,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = "com.example.addressbook")
 public class ApplicationConfig {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
 
 //    @Bean
 //    @Primary
