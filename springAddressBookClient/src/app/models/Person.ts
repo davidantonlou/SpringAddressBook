ac6bf01d79b5897d483fe300106349f3473b7a4b
David Antón <davidantonlou@gmail.com>
2017-07-06 10:29:33 +0200

    initial commit SpringAddressBookClient structure Angular

---
+++ springAddressBookClient/src/app/models/Person.ts
@@ -0,0 +1,16 @@
+import * as models from './models';
+
+export interface Person {
+    id?: number;
+
+    firstName: string;
+
+    lastName: string;
+
+    email?: string;
+
+    mobile?: string;
+
+    company?: string;
+
+}
